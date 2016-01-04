ba817c5bb88dec2c5c0e5aff88f672a73cad088b
alec harmon <alec.blumenfeld@mail.mcgill.ca>
2016-01-04 14:33:23 -0500

    working on classifer

--- CreateClassifiers.py
+++ CreateClassifiers.py
@@ -1,14 +1,27 @@
 from monkeylearn import MonkeyLearn
+from apiKeys import getKey
+from Database import getConservativeText, getLiberalText
+from progressbar import ProgressBar
+pbar = ProgressBar()
 
 # Use the API key from your account
-ml = MonkeyLearn('<YOUR API KEY HERE>')
+ml = MonkeyLearn(getKey())
 
-# Create a new classifier
-res = ml.classifiers.create('Test Classifier')
+text_list = ["The early actions taken by the federal government in the war focused around uniting Americans both in terms of accessibility to one another as well "]
+module_id = 'cl_YK3gBdDK'
+res = ml.classifiers.classify(module_id, text_list, sandbox=True)
+print res.result
 
-# Get the id of the new module
-module_id = res.result['classifier']['hashed_id']
+# # conData = getConservativeText()
+# # conSamples = []
+# # for entry in conData:
+# #     conSamples.append((entry,con_id))
+# # res = ml.classifiers.upload_samples(module_id, conSamples)
 
-# Get the id of the root node
-res = ml.classifiers.detail(module_id)
-root_id = res.result['sandbox_categories'][0]['id']
+
+# # Now let's train the module!
+# res = ml.classifiers.train(module_id)
+
+# # Classify some texts
+# res = ml.classifiers.classify(module_id, ['I love the movie', 'I hate the movie'], sandbox=True)
+# print res.result
